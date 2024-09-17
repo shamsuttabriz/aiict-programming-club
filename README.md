6640fdd12ed28865420cc263463debf18f961eb5
shamsuttabriz <shamsuttabriz.md@gmail.com>
2024-09-17 14:31:09 +0600

    navbar design added but interactivity not added in navbar

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AIICT Programming Club
 
-This is my University's programming club website. I make this website. Here I teach my junior brother on problem-solving, development, and so on using my AIICT Programming club website. I think my university teachers and students will benefit from using my website.
+This is my University's programming club website. I make this website. Here I teach my junior brother on problem-solving, development, and so on using my AIICT Programming club website. I think my university teachers and students will benefit from using my website.I
 
 ## Using Technologies:
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+export default function Navbar() {
+  return (
+    <header className="flex w-full h-auto items-center justify-center sticky top-0 inset-x-0 border-b border-divider backdrop-blur-lg z-50 bg-slate-100/10 py-3.5 dark:bg-slate-800/10">
+      <nav className="w-full lg:w-9/12 px-3 lg:px-0 mx-auto flex justify-between items-center">
+        <div className="flex items-center gap-2 lg:gap-4">
+          <img
+            className="max-w-10"
+            src="../../src/assets/APC-LOGO.png"
+            alt="LOGO"
+          />
+          <span className="text-md lg:text-lg font-bold">
+            AIICT Programming Club
+          </span>
+        </div>
+        <ul className="hidden lg:flex lg:items-center lg:gap-8">
+          <li>
+            <a className="text-md font-bold" href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="text-md font-medium" href="#">
+              Blog
+            </a>
+          </li>
+          <li className="bg-green-600 px-6 py-2 rounded cursor-pointer">
+            <a className="text-md font-medium text-white" href="#">
+              Login
+            </a>
+          </li>
+        </ul>
+        <div className="block lg:hidden cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+            />
+          </svg>
+        </div>
+      </nav>
+    </header>
+  );
+}
