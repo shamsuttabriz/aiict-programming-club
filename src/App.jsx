a1c3d3f688ca69950a4067eae2647af1c41dc314
shamsuttabriz <shamsuttabriz.md@gmail.com>
2024-10-02 15:34:50 +0600

    Hero section added in the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <HeroSection />
     </>
   );
 }
